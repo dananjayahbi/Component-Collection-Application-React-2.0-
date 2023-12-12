9df68c113b63c6063f9f60767500a22bf4554512
djd <dananjayahbi@gmail.com>
2023-12-12 17:24:46 +0530

    Add search functionality to LearningNodes page

--- frontend/src/pages/LearningNodes.jsx
+++ frontend/src/pages/LearningNodes.jsx
@@ -1,24 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Button, Modal, Typography } from 'antd';
+import { Card, Button, Modal, Typography, Input } from 'antd';
 import { EditOutlined, DeleteOutlined, ExclamationCircleOutlined, EyeOutlined } from '@ant-design/icons';
 import { Formik } from 'formik';
 import axios from 'axios';
 import AddLearningNodeModal from './AddLearningNodeModal';
 import EditLearningNodeModal from './EditLearningNodeModal';
-import ViewLearningNodeModal from './ViewLearningNodeModal';  // Add the appropriate path for the ViewLearningNodeModal
+import ViewLearningNodeModal from './ViewLearningNodeModal';  
 
 const { confirm } = Modal;
 
 const LearningNodes = () => {
   const [learningNodes, setLearningNodes] = useState([]);
+  const [filteredNodes, setFilteredNodes] = useState([]);
   const [loading, setLoading] = useState(true);
   const [isLNAddModalVisible, setLNAddModalVisible] = useState(false);
   const [isLNEditModalVisible, setLNEditModalVisible] = useState(false);
   const [viewModalVisible, setViewModalVisible] = useState(false);
   const [selectedNode, setSelectedNode] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    // Fetch learning nodes from the server
     const fetchLearningNodes = async () => {
       try {
         const response = await axios.get('http://localhost:5000/learningNodes/getAllLearningNode');
@@ -31,6 +32,13 @@ const LearningNodes = () => {
 
     fetchLearningNodes();
   }, []);
+
+  useEffect(() => {
+    const filtered = learningNodes.filter(node =>
+      node.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredNodes(filtered);
+  }, [searchTerm, learningNodes]);
 
   const showDeleteConfirm = (record) => {
     confirm({
@@ -50,17 +58,13 @@ const LearningNodes = () => {
   };
 
   const handleEdit = (record) => {
-    // Implement opening the EditLearningNode Modal
-    // You can use state to manage the modal visibility and pass the selected learning node to the modal
     setSelectedNode(record);
     setLNEditModalVisible(true);
   };
 
   const handleDelete = async (record) => {
     try {
-      // Delete learning node
       await axios.delete(`http://localhost:5000/learningNodes/deleteLearningNode/${record._id}`);
-      // Update the learning nodes after deletion
       setLearningNodes((prevNodes) => prevNodes.filter((node) => node._id !== record._id));
     } catch (error) {
       console.error('Error deleting learning node:', error);
@@ -68,25 +72,26 @@ const LearningNodes = () => {
   };
 
   const handleView = (record) => {
-    // Implement opening the ViewLearningNode Modal
-    // You can use state to manage the modal visibility and pass the selected learning node to the modal
     setSelectedNode(record);
     setViewModalVisible(true);
   };
 
   const handleAdd = () => {
-    // Open the AddLearningNodeModal as a modal
     setLNAddModalVisible(true);
   };
 
   const handleAddLNModalSuccess = () => {
-    // Close the AddLearningNodeModal
     setLNAddModalVisible(false);
-  }
+  };
 
   return (
     <div style={{ padding: '20px' }}>
-      <Button type="primary" style={{ marginBottom: '20px' }} onClick={handleAdd}>
+      <Input
+        placeholder="Search by title"
+        onChange={(e) => setSearchTerm(e.target.value)}
+        style={{ marginBottom: '20px', width: '300px' }}
+      />
+      <Button type="primary" style={{ marginBottom: '20px', marginLeft:"10px" }} onClick={handleAdd}>
         Add Learning Node
       </Button>
       <div
@@ -96,7 +101,7 @@ const LearningNodes = () => {
           gap: '16px',
         }}
       >
-        {learningNodes.map((node) => (
+        {(searchTerm ? filteredNodes : learningNodes).map((node) => (
           <Card
             key={node._id}
             style={{
@@ -107,9 +112,9 @@ const LearningNodes = () => {
             }}
           >
             <div>
-              <Typography style={{ marginBottom: '12px', fontWeight:"600" }}>{node.title}</Typography>
+              <Typography style={{ marginBottom: '12px', fontWeight: '600' }}>{node.title}</Typography>
             </div>
-            <div style={{ marginBottom: '12px', display:"flex",justifyContent:"center", alignItems:"center" }}>
+            <div style={{ marginBottom: '12px', display: "flex", justifyContent: "center", alignItems: "center" }}>
               <Button
                 type="primary"
                 icon={<EditOutlined />}
@@ -126,8 +131,7 @@ const LearningNodes = () => {
                 Delete
               </Button>
             </div>
-            {/* Additional card content here */}
-            <div style={{display:"flex",justifyContent:"center", alignItems:"center"}}>
+            <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
               <Button type="default" icon={<EyeOutlined />} onClick={() => handleView(node)}>
                 View
               </Button>
@@ -136,33 +140,30 @@ const LearningNodes = () => {
         ))}
       </div>
 
-      {/* AddLearningNodeModal goes here */}
       <Modal
         visible={isLNAddModalVisible}
         onCancel={() => setLNAddModalVisible(false)}
         footer={null}
         width={"100vw"}
-        style={{top:20}}
+        style={{ top: 20 }}
         destroyOnClose={true}
         maskClosable={false}
       >
         <AddLearningNodeModal onSuccess={handleAddLNModalSuccess} />
       </Modal>
 
-      {/* EditLearningNodeModal goes here */}
       <Modal
         visible={isLNEditModalVisible}
         onCancel={() => setLNEditModalVisible(false)}
         footer={null}
         width={"100vw"}
-        style={{top:20}}
+        style={{ top: 20 }}
         destroyOnClose={true}
         maskClosable={false}
       >
         <EditLearningNodeModal node={selectedNode} />
       </Modal>
 
-      {/* ViewLearningNodeModal goes here */}
       <Modal
         title="View Learning Node"
         visible={viewModalVisible}
